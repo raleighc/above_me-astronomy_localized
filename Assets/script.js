dc4e33b185fc51976f4de2711edfcd23047474f6
Cody Bonsma <codybonsma@gmail.com>
2020-09-16 18:58:11 -0400

    generated api key/pull for uphere api and able to pull the information

--- Assets/script.js
+++ Assets/script.js
@@ -1,7 +1,22 @@
-$(document).ready(function() {
-    console.log("Hello World!");
+$(document).ready(function () {
+  console.log("Hello World!");
 
+  // api link to the top list of satellites from uphere.space 
+  var settings = {
+    async: true,
+    crossDomain: true,
+    url: "https://uphere-space1.p.rapidapi.com/satellite/top?days=1",
+    method: "GET",
+    headers: {
+      "x-rapidapi-host": "uphere-space1.p.rapidapi.com",
+      "x-rapidapi-key": "4b53b200a5msh2b293e52ffd17d9p106b4bjsn85a2dc5edf19",
+    },
+  };
 
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+    var name = response[0].name;
+    console.log(name);
+  });
 
-    
-})
+});
