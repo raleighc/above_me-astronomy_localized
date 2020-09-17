14eae5e45218bfdae38d03ccdf5fd8b569b68223
Cody Bonsma <codybonsma@gmail.com>
2020-09-17 13:16:58 -0400

    added a result page div in html that displays when the on.click event happens for the above me button. The starting-page goes to display none and the result buttons show

--- Assets/script.js
+++ Assets/script.js
@@ -2,36 +2,39 @@ $(document).ready(function () {
   //   console.log("Hello World!");
 
   // grab and connect html elements
-var dayTimeHeader = $("#date-time-header");
-var startBtn = $("#start-btn");
-$("#start-btn").click(function(){
+  var startingPage = $("#starting-page");
+  var resultPage = $("#result-page");
+  var dayTimeHeader = $("#date-time-header");
+  var startBtn = $("#start-btn");
+  $("#start-btn").click(function () {
     console.log("You clicked me!");
-})
+    startingPage.attr("style", "display: none");
+    resultPage.attr("style", "display: block");
+  });
 
   // api link to the top list of satellites from uphere.space
-  function upHereSpace(){
-  var settings = {
-    async: true,
-    crossDomain: true,
-    url: "https://uphere-space1.p.rapidapi.com/satellite/top?days=1",
-    method: "GET",
-    headers: {
-      "x-rapidapi-host": "uphere-space1.p.rapidapi.com",
-      "x-rapidapi-key": "4b53b200a5msh2b293e52ffd17d9p106b4bjsn85a2dc5edf19",
-    },
-  };
+  function upHereSpace() {
+    var settings = {
+      async: true,
+      crossDomain: true,
+      url: "https://uphere-space1.p.rapidapi.com/satellite/top?days=1",
+      method: "GET",
+      headers: {
+        "x-rapidapi-host": "uphere-space1.p.rapidapi.com",
+        "x-rapidapi-key": "4b53b200a5msh2b293e52ffd17d9p106b4bjsn85a2dc5edf19",
+      },
+    };
 
-  $.ajax(settings).done(function (response) {
-    // console.log(response);
-    // grab the satellites name
-    var satName = response[0].name;
-    // console.log(satName);
-    // grab the related satellites number
-    var satNumber = response[0].number;
-    // console.log(satNumber);
-  });
+    $.ajax(settings).done(function (response) {
+      // console.log(response);
+      // grab the satellites name
+      var satName = response[0].name;
+      // console.log(satName);
+      // grab the related satellites number
+      var satNumber = response[0].number;
+      // console.log(satNumber);
+    });
   }
-
 
   // Variable for NeoWs URL with API key.
 
